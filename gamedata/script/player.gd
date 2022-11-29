852b8d637f2dddd6fe216d8ba00cb44d74ca0fb9
Nexurion <acunit26@gmail.com>
2022-11-29 09:19:49 +0300

    wrote a working character controller

--- gamedata/script/player.gd
+++ gamedata/script/player.gd
@@ -1,16 +1,23 @@
 extends KinematicBody2D
 
-var speed := 300
+const GRAVITY = 1000
+const WALK_SPEED = 200
 
-var up_direction := Vector2(0, 0)
-
+var velocity = Vector2()
 
 func _physics_process(delta):
-	var move_direction = G.GRAVITY
-	
-	if Input.is_action_pressed("move_right"):
-		move_direction.x = 1
-	elif Input.is_action_pressed("move_left"):
-		move_direction.x = -1
+	velocity.y += delta * GRAVITY
 
-	move_and_slide(move_direction * speed, up_direction)
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("move_right"):
+		velocity.x =  WALK_SPEED
+	else:
+		velocity.x = 0
+
+	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
+
+	# The second parameter of "move_and_slide" is the normal pointing up.
+	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+	move_and_slide(velocity, Vector2(0, -1))
+
