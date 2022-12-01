88411473ec0e3473a4756612736e91f917aecc0e
Nexurion <acunit26@gmail.com>
2022-12-02 00:10:57 +0300

    added new death areas

--- gamedata/script/death_area.gd
+++ gamedata/script/death_area.gd
@@ -4,3 +4,6 @@ extends Area2D
 func _on_death_out_of_bounds_body_entered(body):
 	if (body.name == ('player')):
 		get_tree().change_scene("res://dev-room.tscn")
+	
+	elif (body.name == ('girl-player')):
+		get_tree().change_scene("res://dev-room.tscn")
